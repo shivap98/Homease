a68a503b7e42e13b1a6f4baca9259639d2dc854e
Kartik Mittal <mittal38@purdue.edu>
2020-09-27 18:46:45 -0400

    Refactoring structure and adding button template

---
+++ homease_frontend/src/components/App.js
@@ -0,0 +1,21 @@
+/**
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import { View, Text } from 'react-native'
+
+
+const App: () => React$Node = () => {
+	return (
+		<>
+			<View></View>
+			<Text>Hello World!</Text>
+		</>
+	);
+};
+
+
+export default App;
