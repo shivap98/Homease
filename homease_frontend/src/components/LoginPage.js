93fe204523a3bc47b9849d963e75ba916c512e54
Sehajbir Randhawa <srandhaw@purdue.edu>
2020-10-04 18:15:27 -0400

    installed firebase

--- homease_frontend/src/components/LoginPage.js
+++ homease_frontend/src/components/LoginPage.js
@@ -6,6 +6,7 @@ import theme from './common/theme';
 import {Button, Provider as PaperProvider, TextInput} from 'react-native-paper';
 import componentStyles from './common/componentStyles';
 import { StackActions } from '@react-navigation/native';
+import firebase from 'firebase';
 
 
 class LoginPage extends Component {
@@ -18,7 +19,7 @@ class LoginPage extends Component {
         super(props);
 
         this._isMounted = true;
-    }
+	}
 
     state = {email: '', password: '', loggedIn: null};
 
@@ -68,11 +69,7 @@ class LoginPage extends Component {
                                         testID="loginButton"
                                         style={{...styles.buttonContainedStyle, margin: 0}}
                                         mode="contained"
-                                        onPress={() => {
-                                            this.props.navigation.dispatch(
-                                                StackActions.replace('Account')
-                                              );
-                                        }}
+                                        onPress={this.onLoginButtonPressed.bind(this)}
                                     >
                                         <Text style={componentStyles.bigButtonTextStyle}>
                                             LOG IN
@@ -110,8 +107,46 @@ class LoginPage extends Component {
                 </PaperProvider>
             </View>
         );
-    }
+	}
+	
+	onLoginButtonPressed() {
+		const {email, password} = this.state;
+		firebase.auth().signInWithEmailAndPassword(email, password)
+			.then(() => {
+					const user = firebase.auth().currentUser;
+					if (user != null) {
+						user.providerData.forEach(function (profile) {
+							console.log("Sign-in provider: " + profile.providerId);
+							console.log("  Provider-specific UID: " + profile.uid);
+							console.log("  Name: " + profile.displayName);
+							console.log("  Email: " + profile.email);
+							console.log("  Photo URL: " + profile.photoURL);
+						});
+					}
+					this.props.navigation.dispatch(resetAction);
+	
+				}
+			)
+			.catch(error => {
+				Alert.alert(
+					'Oops!',
+					error.message,
+					[
+						{
+							text: 'OK',
+							onPress: () => console.log('Cancel Pressed'),
+							style: 'cancel',
+						},
+	
+					],
+					{cancelable: false},
+				);
+				console.log(error)
+			});
+	}
 }
+
+
 
 
 const styles = {
