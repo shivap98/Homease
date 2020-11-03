b853a892817f2d7445c2357b245a345a954222ac
Aman Wali <amanwali27@gmail.com>
2020-11-03 04:26:29 -0500

    Updated Chore Tab. See description
    Cannot convert created chores from recurring to non-recurring and vice-versa. Added Edit button to modify either assigned user or name/description of chore.

--- homease_frontend/src/components/EditChore.js
+++ homease_frontend/src/components/EditChore.js
@@ -22,12 +22,15 @@ class EditChore extends Component{
             {name: 'user3', selected: false}
         ],
         chores: [
-            {key: '1', name: 'Dishes', status: 'incomplete', description: 'Chore 1', recursiveChore: true, selectedUsers: ['user1', 'user3']},
-            {key: '2', name: 'Cleaning', status: 'in progress', description: 'Chore 2', recursiveChore: false, selectedUsers: ['user2']}
+            {key: '1', name: 'Dishes', status: 'incomplete', description: 'Chore 1', recursiveChore: true, selectedUsers: ['user1', 'user3'], currentUser: 'user1', previousUser: 'user3'},
+            {key: '2', name: 'Cleaning', status: 'in progress', description: 'Chore 2', recursiveChore: false, selectedUsers: ['user2'], currentUser: 'user2'}
         ],
         recursiveChore: false,
         description: '',
-        multiLine: true
+        multiLine: true,
+        currentUser: '',
+        previousUser: '',
+        edit: false
     };
 
     constructor(props) {
@@ -41,7 +44,6 @@ class EditChore extends Component{
         console.log(chore.name);
 
         let users = this.state.users;
-        console.log(users);
         users = users.map(user =>{
             if(chore.selectedUsers.includes(user.name)){
                 user.selected = true;
@@ -49,36 +51,52 @@ class EditChore extends Component{
             return user;
         });
         console.log(users);
-        this.setState({choreName: chore.name, description: chore.description, users: users,selectedUsers: chore.selectedUsers, recursiveChore: chore.recursiveChore});
-        this.props.navigation.setOptions({ title: 'Chore' })
+        this.props.navigation.setOptions({ title: 'Chore' });
+        this.setState({choreName: chore.name, description: chore.description, users: users,selectedUsers: chore.selectedUsers, recursiveChore: chore.recursiveChore, currentUser: chore.currentUser, previousUser: chore.previousUser});
     }
 
     onSelectPressed(selectedUser, index){
         console.log("Select pressed");
-        let users = this.state.users;
-        if(this.state.recursiveChore === true) {
-            users[index].selected = !selectedUser.selected;
-            let selectedUsers = this.state.selectedUsers;
-            if(users[index].selected === true){
-                selectedUsers.push(users[index].name);
-            }else{
-                selectedUsers = selectedUsers.filter(user => user !== users[index].name);
-            }
-            this.setState({users: users, selectedUsers: selectedUsers});
-        }else{
-            if(selectedUser.selected === false){
-                console.log("Clicked new user");
-                users = users.map(user => {
-                    user.selected=false;
-                    return user;
-                });
-                users[index].selected = true;
-                let selectedUsers = [];
-                selectedUsers.push(users[index].name);
+        if(this.state.edit === true) {
+            let users = this.state.users;
+            if (this.state.recursiveChore === true) {
+                users[index].selected = !selectedUser.selected;
+                let selectedUsers = this.state.selectedUsers;
+                if (users[index].selected === true) {
+                    selectedUsers.push(users[index].name);
+                } else {
+                    selectedUsers = selectedUsers.filter(user => user !== users[index].name);
+                }
                 this.setState({users: users, selectedUsers: selectedUsers});
-            }else{
-                console.log("Clicked selected user again");
+            } else {
+                if (selectedUser.selected === false) {
+                    console.log("Clicked new user");
+                    users = users.map(user => {
+                        user.selected = false;
+                        return user;
+                    });
+                    users[index].selected = true;
+                    let selectedUsers = [];
+                    selectedUsers.push(users[index].name);
+                    this.setState({users: users, selectedUsers: selectedUsers}); //UPDATE CURRENT USER FIELD ONLY IF THEY SAVE CHANGES
+                } else {
+                    console.log("Clicked selected user again");
+                }
             }
+        }else{
+            Alert.alert(
+                'Oops!',
+                'Click the edit button to update chore assignments',
+                [
+                    {
+                        text: 'OK',
+                        onPress: () => {},
+                        style: 'cancel',
+                    },
+
+                ],
+                {cancelable: false},
+            );
         }
     }
 
@@ -90,28 +108,17 @@ class EditChore extends Component{
                     title={item.name}
                     titleStyle={(item.selected) ? styles.selectedTextStyle : styles.unselectedTextStyle}
                     key={index}
-                    onPress={() => {this.onSelectPressed(item, index)}}
+                    onPress={() => {
+                        this.onSelectPressed(item, index);
+                    }}
                 />
             )
         })
-
     }
 
-    onRecursiveClicked(){
-        console.log("data check in props");
-        console.log(this.props.route.params.key);
-        if(this.state.recursiveChore === true){
-            let users = this.state.users;
-            users = users.map(user => {
-                user.selected=false;
-                return user;
-            });
-            console.log("Setting recursiveChore to false");
-            let selectedUsers = [];
-            this.setState({users: users, recursiveChore: false, selectedUsers: selectedUsers})
-        }else{
-            this.setState({recursiveChore: true})
-        }
+    onEditClicked(){
+        console.log("edit was "+this.state.edit);
+        this.setState({edit: !this.state.edit})
     }
 
     onCreateChoreClicked(){
@@ -135,19 +142,62 @@ class EditChore extends Component{
         }
     }
 
+    onRollBackClicked(){
+        console.log("ROLLBACK CLICKED");
+    }
+
+    renderPreviousUser (){
+        let previousUser = this.state.previousUser;
+        return (
+            <View>
+                <Text style={styles.unselectedTextStyle}>
+                    {previousUser}
+                </Text>
+                <Button
+                    color={theme.buttonColor}
+                    style={styles.buttonContainedStyle}
+                    mode="contained"
+                    onPress={() => {this.onRollBackClicked()}}
+                >
+                    ROLLBACK CHORE TO USER
+                </Button>
+            </View>
+        )
+    }
+
+    showPreviousUser(){
+        if(this.state.recursiveChore === true){
+            return (
+                <List.Accordion
+                    title="LAST COMPLETED BY"
+                    onPress={() => {LayoutAnimation.easeInEaseOut()}}
+                >
+                    {this.renderPreviousUser()}
+                </List.Accordion>
+            )
+        }
+    }
+
+    showGroupMembers(){
+        return(
+            <View style={componentStyles.cardSectionWithBorderStyle}>
+                <Text style={styles.cardHeaderTextStyle}>GROUP MEMBERS</Text>
+                <List.Accordion
+                    title="List of members in group"
+                    onPress={() => {LayoutAnimation.easeInEaseOut()}}
+                >
+                    {this.renderListOfMembers()}
+                </List.Accordion>
+            </View>
+        )
+    }
+
     render() {
         return (
             <View style={{flex: 1, backgroundColor: theme.backgroundColor}}>
                 <PaperProvider theme={paperTheme}>
                     <ScrollView>
                         <View style={styles.viewStyle}>
-                            <TextInput
-                                style={styles.textInputStyle}
-                                label='Chore Name'
-                                mode='outlined'
-                                value={this.state.choreName}
-                                onChangeText={textString => this.setState({choreName: textString})}
-                            />
                             <CardSection>
                                 <Text style={{
                                     fontWeight: 'bold',
@@ -155,34 +205,60 @@ class EditChore extends Component{
                                     marginTop: 7,
                                     marginRight: 15,
                                     color: 'white',
-                                    textAlign: 'center'
+                                    textAlign: 'right'
                                 }}>
-                                    Recursive chore?
+                                    EDIT
                                 </Text>
                                 <Switch
-                                    value={this.state.recursiveChore}
-                                    onValueChange={() => {
-                                        this.onRecursiveClicked();
-                                    }}
+                                    value={this.state.edit}
+                                    onValueChange={() => {this.onEditClicked()}}
                                 />
                             </CardSection>
-                            <View style={componentStyles.cardSectionWithBorderStyle}>
-                                <Text style={styles.cardHeaderTextStyle}>GROUP MEMBERS</Text>
-                                <List.Accordion
-                                    title="List of members in group"
-                                    onPress={() => {LayoutAnimation.easeInEaseOut()}}
-                                >
-                                    {this.renderListOfMembers()}
-                                </List.Accordion>
-                            </View>
+                            <TextInput
+                                style={styles.textInputStyle}
+                                label='Chore Name'
+                                mode='outlined'
+                                theme={{
+                                    colors: {
+                                        placeholder: this.state.edit ? 'white' : theme.lightColor,
+                                        text: this.state.edit ? 'white' : theme.lightColor,
+                                        primary: this.state.edit ? 'white' : theme.lightColor,
+                                    }
+                                }}
+                                value={this.state.choreName}
+                                onChangeText={textString => this.setState({choreName: textString})}
+                                editable={this.state.edit}
+                            />
                             <TextInput
                                 style={styles.textInputStyle}
                                 label='Chore Description'
                                 mode='outlined'
+                                theme={{
+                                    colors: {
+                                        placeholder: this.state.edit ? 'white' : theme.lightColor,
+                                        text: this.state.edit ? 'white' : theme.lightColor,
+                                        primary: this.state.edit ? 'white' : theme.lightColor,
+                                    }
+                                }}
                                 multiline= {true}
                                 value={this.state.description}
                                 onChangeText={textString => this.setState({description: textString})}
+                                editable={this.state.edit}
                             />
+                            {this.showGroupMembers()}
+                            <View style={componentStyles.cardSectionWithBorderStyle}>
+                                <Text style={styles.cardHeaderTextStyle}>ASSIGNMENT INFORMATION</Text>
+                                <TextInput
+                                    style={styles.textInputStyle}
+                                    label='Currently assigned user'
+                                    mode='outlined'
+                                    value={this.state.currentUser}
+                                    keyboardAppearance='dark'
+                                    editable={false}
+                                    onChangeText={textString => {}}
+                                />
+                                {this.showPreviousUser()}
+                            </View>
                             <Button
                                 color={theme.buttonColor}
                                 style={styles.buttonContainedStyle}
